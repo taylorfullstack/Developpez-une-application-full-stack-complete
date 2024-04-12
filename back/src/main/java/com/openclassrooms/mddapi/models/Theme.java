bb7a335faf73a676f7987c09a63ab245323bbf40
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-04-12 15:25:22 +0200

    Theme feature (#2)
    * :arrow_up: upgrade Typescript version
    
    * :arrow_up: upgrade Angular from v14 to v15
    
    * :arrow_up: upgrade Angular Material version
    
    * :arrow_up: upgrade Typescript version
    
    * :arrow_up: upgrade Angular from v15 to v16
    
    * :wrench: update tsconfig.json skipLibCheck
    
    * :arrow_up: update Typescript to latest
    
    * :arrow_up: upgrade Angular from v16 to v17
    
    * :arrow_up: update Angular Material to v16
    
    * :arrow_up: prepare Angular Material update
    
    * :arrow_up: update Angular Material to v17.3.1
    
    * :lock: updated package-lock, npm audit fix
    
    * :arrow_up: update rxjs and test dependencies
    
    * :see_no_evil: add a global gitignore file
    
    * :see_no_evil: add .env to gitignore
    
    * :arrow_up: upgrade SpringBoot and Java to latest versions
    
    * :card_file_box: add database configuration
    
    * :sparkles: add HomeController to handle root endpoint
    
    * :heavy_plus_sign: add Lombok
    
    * :wrench: update application configuration variables
    
    * :heavy_plus_sign: add security, database, mapping, and documentation dependencies to pom.xml
    
    * :construction: add initial security configuration
    
    * :sparkles: add themes feature
    
    * :sparkles: add navbar component
    
    * :card_file_box: add sql script for sample data
    
    * :construction: update home component
    
    * :lipstick: update global styles and import google fonts

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/Theme.java
@@ -0,0 +1,42 @@
+package com.openclassrooms.mddapi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "THEMES")
+@Data
+@Accessors(chain = true)
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Theme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "theme")
+    private List<Article> articles = new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+}
---
+++ back/src/main/resources/sql/mdd.sql
@@ -0,0 +1,63 @@
+CREATE TABLE USERS (
+                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                       name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE THEMES (
+                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                        title VARCHAR(255) NOT NULL,
+                        description VARCHAR(255) NOT NULL,
+                        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ARTICLES (
+                          id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                          title VARCHAR(255) NOT NULL,
+                          content TEXT NOT NULL,
+                          user_id BIGINT NOT NULL,
+                          theme_id BIGINT NOT NULL,
+                          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          FOREIGN KEY (user_id) REFERENCES USERS(id),
+                          FOREIGN KEY (theme_id) REFERENCES THEMES(id)
+);
+
+CREATE TABLE COMMENTS (
+                          id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                          content TEXT NOT NULL,
+                          user_id BIGINT NOT NULL,
+                          article_id BIGINT NOT NULL,
+                          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          FOREIGN KEY (user_id) REFERENCES USERS(id),
+                          FOREIGN KEY (article_id) REFERENCES ARTICLES(id)
+);
+
+-- Inserting user
+INSERT INTO USERS(name, email, password) VALUES ('Heather Taylor', 'ht@mdd.com', 'pass1234');
+
+-- Inserting themes
+INSERT INTO THEMES(title, description) VALUES ('Elixir', 'Elixir is a dynamic, functional language designed for building scalable and maintainable applications.');
+INSERT INTO THEMES(title, description) VALUES ('Phoenix', 'Phoenix is a web development framework written in Elixir which implements the server-side Model View Controller (MVC) pattern.');
+INSERT INTO THEMES(title, description) VALUES ('Phoenix LiveView', 'Phoenix LiveView is a library for Phoenix Framework that enables rich, real-time user experiences with server-rendered HTML.');
+INSERT INTO THEMES(title, description) VALUES ('Angular', 'Angular is a platform for building mobile and desktop web applications.');
+INSERT INTO THEMES(title, description) VALUES ('Java', 'Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.');
+INSERT INTO THEMES(title, description) VALUES ('JavaScript', 'JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive.');
+INSERT INTO THEMES(title, description) VALUES ('TypeScript', 'TypeScript is a strongly typed, object oriented, compiled language.');
+INSERT INTO THEMES(title, description) VALUES ('Python', 'Python is an interpreted, high-level and general-purpose programming language.');
+INSERT INTO THEMES(title, description) VALUES ('React', 'React is an open-source, front end, JavaScript library for building user interfaces or UI components.');
+INSERT INTO THEMES(title, description) VALUES ('Django', 'Django is a high-level Python web framework that enables rapid development of secure and maintainable websites.');
+INSERT INTO THEMES(title, description) VALUES ('CSS', 'CSS is a style sheet language used for describing the look and formatting of a document written in HTML.');
+INSERT INTO THEMES(title, description) VALUES ('HTML', 'HTML is the standard markup language for documents designed to be displayed in a web browser.');
+
+-- Inserting articles
+INSERT INTO ARTICLES(title, content, user_id, theme_id) VALUES ('Elixir Article', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1, (SELECT id FROM THEMES WHERE title = 'Elixir'));
+INSERT INTO ARTICLES(title, content, user_id, theme_id) VALUES ('Phoenix Article', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1, (SELECT id FROM THEMES WHERE title = 'Phoenix'));
+INSERT INTO ARTICLES(title, content, user_id, theme_id) VALUES ('CSS Article', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1, (SELECT id FROM THEMES WHERE title = 'CSS'));
+INSERT INTO ARTICLES(title, content, user_id, theme_id) VALUES ('HTML Article', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1, (SELECT id FROM THEMES WHERE title = 'HTML'));
+INSERT INTO ARTICLES(title, content, user_id, theme_id) VALUES ('TypeScript Article', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1, (SELECT id FROM THEMES WHERE title = 'TypeScript'));

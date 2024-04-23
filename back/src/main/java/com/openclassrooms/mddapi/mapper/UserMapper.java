0e3a8c889d76adf8674fa7f0de317f5d43e0e8a5
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-04-23 14:11:30 +0200

    :ambulance: fix UserMapper

--- back/src/main/java/com/openclassrooms/mddapi/mapper/UserMapper.java
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/UserMapper.java
@@ -2,29 +2,10 @@ package com.openclassrooms.mddapi.mapper;
 
 import com.openclassrooms.mddapi.dto.UserDTO;
 import com.openclassrooms.mddapi.models.User;
-import com.openclassrooms.mddapi.models.Theme;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    @Mapping(source = "subscribedThemes", target = "subscribedThemeIds", qualifiedByName = "themesToLongs")
     UserDTO toDTO(User user);
     User toEntity(UserDTO userDTO);
-
-    @Named("themesToLongs")
-    default List<Long> themesToLongs(List<Theme> themes) {
-        return themes.stream()
-                .map(this::themeToLong)
-                .collect(Collectors.toList());
-    }
-
-    default Long themeToLong(Theme theme) {
-        return theme.getId();
-    }
 }

187d293e9a7cce492f527adf50e915b3f0f0fbc4
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-04-25 22:14:14 +0200

    :construction: add Login, Register, and Me component to app routing module, need to reimplement auth guards

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,15 +5,24 @@ import { ListComponent as ArticleListComponent } from "./features/articles/compo
 import { ListComponent as ThemeListComponent } from "./features/themes/components/list/list.component";
 import {FormComponent as ArticleFormComponent} from "./features/articles/components/form/form.component";
 import {DetailComponent as ArticleDetailComponent} from "./features/articles/components/detail/detail.component";
+import { AuthGuard } from './features/auth/guards/auth.guard';
+import { UnauthGuard } from './features/auth/guards/unauth.guard';
+import {RegisterComponent} from "./features/auth/register/register.component";
+import {LoginComponent} from "./features/auth/login/login.component";
+import {MeComponent} from "./features/me/components/me/me.component";
 
-// TODO: add a guard combined with canLoad / canActivate route option to manage unauthenticated user to access private routes
+
+// TODO: put auth guards back once backend auth error is resolved
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'articles', component: ArticleListComponent },
-  { path: 'articles/new', component: ArticleFormComponent},
-  { path: 'articles/:id', component: ArticleDetailComponent },
-  { path: 'themes', component: ThemeListComponent },
-  { path: 'profile', component: HomeComponent }
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'articles', component: ArticleListComponent}, //, canActivate: [AuthGuard]
+  { path: 'articles/new', component: ArticleFormComponent}, //, canActivate: [AuthGuard]
+  { path: 'articles/:id', component: ArticleDetailComponent}, //, canActivate: [AuthGuard]
+  { path: 'themes', component: ThemeListComponent}, //, canActivate: [AuthGuard]
+  { path: 'profile', component: MeComponent}
 ];
 
 @NgModule({

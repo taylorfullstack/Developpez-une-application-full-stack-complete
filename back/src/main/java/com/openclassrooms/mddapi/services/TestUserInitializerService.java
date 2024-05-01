0f6bfecc27f439583a04851f629013b93dfa13d7
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-05-01 16:53:12 +0200

    :technologist: add test user with hashed password on startup

--- back/src/main/java/com/openclassrooms/mddapi/services/TestUserInitializerService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/TestUserInitializerService.java
@@ -1,36 +1,39 @@
-//package com.openclassrooms.mddapi.services;
-//
-//import com.openclassrooms.mddapi.models.User;
-//import com.openclassrooms.mddapi.repository.UserRepository;
-//import org.springframework.context.event.ContextRefreshedEvent;
-//import org.springframework.context.event.EventListener;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-////@Service
-//public class TestUserInitializerService {
-//
-//    private final UserRepository userRepository;
-//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-//
-//    public TestUserInitializerService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-//        this.userRepository = userRepository;
-//        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-//    }
-//
-//    @EventListener
-//    public void onApplicationEvent(ContextRefreshedEvent ignoredEvent) {
-//        Optional<User> userOptional = userRepository.findAll().stream()
-//                .filter(user -> user.getEmail().equals("user@mdd.com"))
-//                .findFirst();
-//
-//        if (userOptional.isPresent()) {
-//            User user = userOptional.get();
-//
-//            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-//            userRepository.save(user);
-//        }
-//    }
-//}
+package com.openclassrooms.mddapi.services;
+
+import com.openclassrooms.mddapi.models.User;
+import com.openclassrooms.mddapi.repository.UserRepository;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class TestUserInitializerService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public TestUserInitializerService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @EventListener
+    public void onApplicationEvent(ContextRefreshedEvent ignoredEvent) {
+        String testEmail = "test@mdd.com";
+        User userFromDb = userRepository.findByEmail(testEmail);
+        Optional<User> existingUser = Optional.ofNullable(userFromDb);
+
+        if (!existingUser.isPresent()) {
+            User user = new User();
+            user.setUsername("testuser");
+            user.setEmail(testEmail);
+            user.setPassword(bCryptPasswordEncoder.encode("Mdd@123!"));
+            userRepository.save(user);
+        }
+    }
+
+}
